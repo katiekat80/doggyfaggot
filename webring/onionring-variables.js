85e9901462d47e884f2860c9505645aa50fb9d48
katiekat80 <139665499+katiekat80@users.noreply.github.com>
2025-12-21 02:45:58 -0600

    trying to fix webring

--- webring/onionring-variables.js
+++ webring/onionring-variables.js
@@ -18,7 +18,7 @@ var ringName = 'Animals Who Can Do Math';
 /* the unique ID of the widget. two things to note:
  1) make sure there are no spaces in it - use dashes or underscores if you must
  2) remember to change 'webringid' in the widget code you give out and all instances of '#webringid' in the css file to match this value!*/
-var ringID = 'doggyfagring';
+var ringID = 'dogfagring';
 
 //should the widget include a link to an index page?
 var useIndex = true;
